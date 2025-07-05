547500d009b5a042b9b0f7fb60356c4d42b3ac9e
Praveen Allam <praveenallam93@gmail.com>
2025-07-05 10:29:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,66 @@
-def main():
-    print("Hello from files-backend!")
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
+from config.settings import settings
+from config.minio_config import minio_client
+from config.redis_config import pool
+
+import time
+from routers import router_modules
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    
+    if not minio_client.bucket_exists(settings.MINIO_BUCKET.lower().replace("_", "-")):
+        minio_client.make_bucket(settings.MINIO_BUCKET.lower().replace("_", "-"))
+        
+    if not minio_client.bucket_exists(settings.MINIO_QUARANTINE_BUCKET.lower().replace("_", "-")):
+        minio_client.make_bucket(settings.MINIO_QUARANTINE_BUCKET.lower().replace("_", "-"))
+        
+    yield
+    
+    
+app = FastAPI(
+    title= settings.PROJECT_NAME,
+    description='NA',
+    version = "0.1.0",
+    docs_url='/docs' if settings.DEBUG else None,
+    redoc_url='/redoc' if settings.DEBUG else None,
+    contact={
+        "name": "Praveen Allam",
+        "email": "saipraveen.allam@copart.com"
+    },
+    debug= settings.DEBUG,
+    lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = settings.BACKEND_CORS_ORIGINS,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+@app.get("/health", tags=["Health Check"])
+def health_check() -> dict:
+    
+    health_data = {"status" : "healthy", "minio_status": "unkown"}
+    
+    try:
+        buckets = minio_client.list_buckets()
+        health_data["minio_status"] = "healthy" if buckets else "unhealthy"
+    except Exception as e:
+        health_data["minio_status"] = "unhealthy"
+    
+    return health_data
+
+for router_module in router_modules:
+    app.include_router(router_module.router)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("app:main", host = "0.0.0.0", port = 8000, reload = True)
